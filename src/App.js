585021f963f853d00ef3d98021f9a186ec8b090e
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-04-22 19:56:51 -0300

    Added the exercise logic to the app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
+class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: "",
+      senha: "",
+      userValido: "epafimonas@mail.com",
+      senhaValida: "teste123",
+      autenticado: undefined
+    }
+    this.muda.bind(this)
+    this.autenticar = this.autenticar.bind(this)
+    this.renderAutenticacao = this.renderAutenticacao.bind(this)
+  }
+
+  muda(event, key) {
+    let state = this.state;
+    if (key === "email") {
+      state.email = event.target.value;
+    }
+    else if (key === "senha") {
+      state.senha = event.target.value;
+    }
+    this.setState(state);
+  }
+
+  autenticar() {
+    let state = this.state;
+    if (state.email === state.userValido && state.senha === state.senhaValida) {
+      state.autenticado = true;
+    }
+    else {
+      state.autenticado = false;
+    }
+    this.setState(state);
+  }
+
+  renderAutenticacao() {
+    if(this.state.autenticado != undefined){
+      if (this.state.autenticado)
+        return (<div>Acessado com sucesso!</div>)
+      else
+        return (<div>Usuário ou senha incorretos!</div>)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <input type='text' size="20" name="email" onChange={(e) => this.muda(e, "email")}></input>
+        </div>
+        <div>
+          <input type='password' size="20" name="senha" onChange={(e) => this.muda(e, "senha")}></input>
+        </div>
+        <button onClick={this.autenticar}>Acessar</button>
+        <this.renderAutenticacao></this.renderAutenticacao>
+      </div>)
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App-header'>
+      <h1 className='App-header'>Login</h1>
+      <Login></Login>
     </div>
   );
 }
